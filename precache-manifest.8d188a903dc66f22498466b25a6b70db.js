9fccb1023f8494bb0866b8fca9a511d499bc3236
Rijita <shrestharijita@gmail.com>
2020-10-24 00:24:09 +1100

    Updates

---
+++ precache-manifest.8d188a903dc66f22498466b25a6b70db.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "183c208899a1506c87cb8b9ebbbeed1b",
+    "url": "/Reactweb/index.html"
+  },
+  {
+    "revision": "e6190e2f3e265e3ff04f",
+    "url": "/Reactweb/static/css/2.ea6b7726.chunk.css"
+  },
+  {
+    "revision": "47e324efaaec3d0321a6",
+    "url": "/Reactweb/static/css/main.5cbfb94e.chunk.css"
+  },
+  {
+    "revision": "e6190e2f3e265e3ff04f",
+    "url": "/Reactweb/static/js/2.91e7ec5a.chunk.js"
+  },
+  {
+    "revision": "d877b083b942576a955038ed13c1c8e7",
+    "url": "/Reactweb/static/js/2.91e7ec5a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "47e324efaaec3d0321a6",
+    "url": "/Reactweb/static/js/main.bc9b741f.chunk.js"
+  },
+  {
+    "revision": "8577305d902f5d5aca6c",
+    "url": "/Reactweb/static/js/runtime-main.88924c6a.js"
+  },
+  {
+    "revision": "970d6510fdce865754245fe418bc19f0",
+    "url": "/Reactweb/static/media/angel.970d6510.png"
+  },
+  {
+    "revision": "b82d44d60ccb54dc9f8ac463ee1542c3",
+    "url": "/Reactweb/static/media/mini.b82d44d6.png"
+  },
+  {
+    "revision": "39233dbf2e9c20e34e0befa0d51bd761",
+    "url": "/Reactweb/static/media/ps.39233dbf.jpg"
+  },
+  {
+    "revision": "42fdde7e93528a67ea6b33450c2dac32",
+    "url": "/Reactweb/static/media/train.42fdde7e.jpg"
+  },
+  {
+    "revision": "0fb1c8291ca93a7b61da38fb61b1ef2d",
+    "url": "/Reactweb/static/media/web.0fb1c829.png"
+  }
+]);
